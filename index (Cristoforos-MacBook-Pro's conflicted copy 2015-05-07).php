fde6d7bb11713dbf6d7821de57497b6fa5372735
Cristoforo Magliozzi <CMagliozzi@post.harvard.edu>
2015-05-07 19:37:28 -0400

    spacing

--- index (Cristoforos-MacBook-Pro's conflicted copy 2015-05-07).php
+++ index (Cristoforos-MacBook-Pro's conflicted copy 2015-05-07).php
@@ -169,14 +169,14 @@
                     <h2>Radio</h2>
                     <hr><br><br>
                     <h4>You can hear my audio stories on public radio shows and podcasts.</h4>
-                    <p style="margin-bottom: 35px;"><a href="http://99percentinvisible.org/episode/tunnel-57/"><img src="img/99invisible.jpg" alt="99% Invisible" style="width:730px" target="_blank"></a></p> 
+                    <p><a href="http://99percentinvisible.org/episode/tunnel-57/"><img src="img/99invisible.jpg" alt="99% Invisible" style="width:730px" target="_blank"></a></p> 
                     <p> 99% Invisible. “Tunnel 57.”
 	                    <audio controls>
 	                    	<source src="snds/Tunnel_57.mp3" type="audio/mpeg">
 	                    	Your browser does not support the audio element. 
 	                    </audio>
+	                    <div style="font-size: 14px; text-align: left; margin-top: 0; margin-bottom: 10 px; padding-top: 0">Producer: Daniel A. Gross. Editing and Scoring: Roman Mars and Sam Greenspan.</div>
                     </p>
-                    <div style="font-size: 14px; text-align: left; margin-top: 0; margin-bottom: 10 px; padding-top: 0">Producer: Daniel A. Gross. Editing and Scoring: Roman Mars and Sam Greenspan.</div>
                     <p>KCRW's UnFictional. “Bookbinder.”
                     <audio controls>
 	                    	<source src="snds/Bookbinder.mp3" type="audio/mpeg">
